e506729d69a90e424cc66ec57088fe3601082a28
abdullohasia <tolaganova624@gmail.com>
2024-03-18 20:39:02 +0500

    bu navbar

--- src/components/UI/Header/index.jsx
+++ src/components/UI/Header/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Link from 'next/link'
+import scr from './style.module.scss'
+
+// icon 
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const Header = () => {
   return (
-    <div className='container'>
-     <h1> Header</h1>
+    <div className={scr.header}>
+     <div className="container">
+       <nav>
+         <img src='/images/logo.svg' alt="" />
+         <ul>
+         <li><Link href='favourite'><FavoriteBorderIcon/></Link></li>
+         <li><Link href='favourite'><FavoriteBorderIcon/></Link></li>
+         <li><Link href='favourite'><FavoriteBorderIcon/></Link></li>
+         <li><Link href='favourite'><FavoriteBorderIcon/></Link></li>
+         <button>E'lon berish</button>
+         </ul>
+       </nav> 
+     </div>
     </div>
   )
 }
