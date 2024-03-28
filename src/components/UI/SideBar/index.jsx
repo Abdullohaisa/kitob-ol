e9fb703f638bf47c7be2e09fe64e59ab3670c196
abdullohasia <tolaganova624@gmail.com>
2024-03-28 16:23:21 +0500

    sideBar

--- src/components/UI/SideBar/index.jsx
+++ src/components/UI/SideBar/index.jsx
@@ -1,110 +1,36 @@
-import s from './style.module.scss'
-import React, { useState } from 'react';
+import s from "./style.module.scss";
+import React, { useState } from "react";
 
 // ICON
-import ClearIcon from '@mui/icons-material/Clear';
-import MenuIcon from '@mui/icons-material/Menu';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ClearIcon from "@mui/icons-material/Clear";
+import MenuIcon from "@mui/icons-material/Menu";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import Link from "next/link";
 
-// import { styled } from '@mui/material/styles';
-// import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-// import MuiAccordion from '@mui/material/Accordion';
-// import MuiAccordionSummary from '@mui/material/AccordionSummary';
-// import MuiAccordionDetails from '@mui/material/AccordionDetails';
-// import Typography from '@mui/material/Typography';
-
-const SideBar = ({bar, close}) => {
-
-    // const Accordion = styled((props) => (
-    //     <MuiAccordion disableGutters elevation={0} square {...props} />
-    //   ))(({ theme }) => ({
-    //     border: `1px solid ${theme.palette.divider}`,
-    //     '&:not(:last-child)': {
-    //       borderBottom: 0,
-    //     },
-    //     '&::before': {
-    //       display: 'none',
-    //     },
-    //   }));
-      
-    //   const AccordionSummary = styled((props) => (
-    //     <MuiAccordionSummary
-    //       expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
-    //       {...props}
-    //     />
-    //   ))(({ theme }) => ({
-    //     backgroundColor:
-    //       theme.palette.mode === 'dark'
-    //         ? 'rgba(255, 255, 255, .05)'
-    //         : 'rgba(0, 0, 0, .03)',
-    //     flexDirection: 'row-reverse',
-    //     '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    //       transform: 'rotate(90deg)',
-    //     },
-    //     '& .MuiAccordionSummary-content': {
-    //       marginLeft: theme.spacing(1),
-    //     },
-    //   }));
-      
-    //   const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-    //     padding: theme.spacing(2),
-    //     borderTop: '1px solid rgba(0, 0, 0, .125)',
-    //   }));
-
-    //   const [expanded, setExpanded] = useState('panel1');
-      
-    //   const handleChange = (panel) => (event, newExpanded) => {
-    //     setExpanded(newExpanded ? panel : false);
-    
+const SideBar = ({ bar, close }) => {
   return (
-    <div className={ bar ? s.openBar : s.bar}>
+    <div className={bar ? s.openBar : s.bar}>
       <div>
-       <ul>
-       <img src='/images/logo.svg' alt="Logo" />
-        <button onClick={(() => close())}>{ bar ? <ClearIcon/> : <MenuIcon/> }</button>
-       </ul>
+        <ul>
+          <img src="/images/logo.svg" alt="Logo" />
+          <button onClick={() => close()}>
+            {bar ? <ClearIcon /> : <MenuIcon />}
+          </button>
+        </ul>
+       <Link href='announce.registration'><p><span>E’lon berish</span></p>
+       </Link>
         <p>
-
-            <span>E’lon berish</span>
+          <FavoriteBorderIcon />
+          <span>Sevimlilar</span>
         </p>
         <p>
-            <FavoriteBorderIcon/>
-            <span>Sevimlilar</span>
+          <NotificationsNoneIcon />
+          <span>Bildirishnomalar</span>
         </p>
-        <p>
-            <NotificationsNoneIcon/>
-            <span>Bildirishnomalar</span>
-        </p>
-        {/* <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography>Collapsible Group Item #1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography>Collapsible Group Item #2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
